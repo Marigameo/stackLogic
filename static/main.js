d9314469798dafc17c47994c26ef8ef7c001b90e
Mariappan S <mariappan@facilio.com>
2020-07-16 13:54:57 +0530

    initial commit

---
+++ static/main.js
@@ -0,0 +1,55 @@
+//get refresh button
+let refreshBtn = document.getElementById("refresh");
+
+let stackBalls = () => {
+  document.getElementById("box").innerHTML = "";
+
+  let inputRows = document.getElementById("noOfRows");
+  let noOfRows = inputRows.value;
+
+  let inputBalls = document.getElementById("noOfBalls");
+  let noOfBalls = inputBalls.value;
+
+  if (!noOfRows) {
+    alert("Please enter a valid input for number of rows");
+  } else if (!noOfBalls) {
+    alert("Please enter a valid input for number of balls");
+  } else {
+    let message = document.getElementById("outputMessage");
+    message.innerHTML = `Stacking ${noOfRows} rows each with ${noOfBalls} balls`;
+    message.classList.add("message");
+    message.style.visibility = "visible";
+    let box = document.getElementById("box");
+    for (let i = 0; i < noOfRows; i++) {
+      let row = document.createElement("div");
+      for (let j = 0; j < noOfBalls; j++) {
+        let ball = document.createElement("div");
+        ball.classList.add("circle");
+        ball.style.width = 600 / noOfBalls + "px";
+        ball.style.height = 600 / noOfBalls + "px";
+        row.appendChild(ball);
+      }
+      box.appendChild(row);
+    }
+    refreshBtn.disabled = false;
+  }
+};
+
+let refresh = () => {
+  document.getElementById("noOfRows").value = null;
+  document.getElementById("noOfBalls").value = null;
+  document.getElementById("box").innerHTML = "";
+  document.getElementById("outputMessage").style.visibility = "hidden";
+  refreshBtn.disabled = true;
+};
+
+//validation
+let validate = e => {
+  if (e.value < 1) {
+    alert("Please enter a value greater than zero");
+    e.value = null;
+  }
+};
+
+//refresh button disabled by default
+refreshBtn.disabled = true;
